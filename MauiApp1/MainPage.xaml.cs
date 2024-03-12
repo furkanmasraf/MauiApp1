9ad4decb1048de260733669a62aa1c345a70b01b
furkanmasraf <furkanmasraf@FURKANMASRA088F>
2024-03-12 13:35:59 +0300

    Add project files.

---
+++ MauiApp1/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿namespace MauiApp1
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Add_Button_Clicked(object sender, EventArgs e)
+        {
+            float X, Y, Z;
+
+            float.TryParse(XX.Text, out X);
+            float.TryParse(YY.Text, out Y);
+            Z = X + Y;
+            ZZ.Text = Z.ToString();
+
+
+
+        }
+
+    }
+}
